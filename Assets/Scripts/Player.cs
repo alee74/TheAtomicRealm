7ac315e97cc46c1b8817b0ca7d53901b59dbbd64
alee74 <alee5@trinity.edu>
2017-10-05 17:42:43 -0500

    My First Commit!
    Built a level by hand to move around in, created prefabs of wall and floor objects, wrote script for player movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private Rigidbody2D atom;
+    private Animator atomSpin;
+    
+    // Use this for initialization
+    void Start () {
+
+        atom = GetComponent<Rigidbody2D>();
+        atomSpin = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inX = Input.GetAxis("HorizontalMove");
+        atom.velocity = new Vector2(inX * speed, atom.velocity.y);
+
+        float inY = Input.GetAxis("VerticalMove");
+        atom.velocity = new Vector2(atom.velocity.x, inY * speed);
+
+
+	}
+}
